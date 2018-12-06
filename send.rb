1987e7a9f65d56aa920a123d38999b601e7a1c46
Patrick <patrick.edward.alcindor@gmail.com>
2018-12-06 12:43:56 -0500

    first commit

---
+++ send.rb
@@ -0,0 +1,28 @@
+require 'action_mailer'
+
+ActionMailer::Base.view_paths= File.dirname(__FILE__)
+ActionMailer::Base.smtp_settings = {
+    address:    'smtp.gmail.com',
+    port:       '587',
+    user_name:  ENV['EMAIL_USERNAME'],
+    password:    ENV['EMAIL_PASSWORD'],
+    authentication: :plain
+}
+
+class Newsletter < ActionMailer::Base
+    default from: 'nycdatest@gmail.com'
+
+    def welcome(recipient)
+        @recipient = recipient
+        mail(to: recipient, 
+            subject: "[Signed up] Welcome #{recipient}")
+    end
+end
+
+Newsletter.welcome("adeolatemitayo@gmail.com").deliver_now
+
+#You need a folder whose name matches the name of the class from
+#actionmailer base
+
+#In that folder
+#you need at least 2 files (function).html.erb
