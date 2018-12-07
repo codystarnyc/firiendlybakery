47497b811635adf0c6a886a5bd6bd92052e0593b
Patrick <patrick.edward.alcindor@gmail.com>
2018-12-07 00:50:22 -0500

    fixing navigation issues

--- server.rb
+++ server.rb
@@ -1,10 +1,6 @@
-
 require 'sinatra'
 require 'curb'
 require 'json'
-
-
-
 
 get '/' do
   erb :home
@@ -27,13 +23,6 @@ get '/contact' do
   erb :contact
 end
 
-# get '/events' do
-#   @result = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=baking&location.address=nyc&token=44ZRBDN4HYITKNUPIVV7")
-#   @data = @result.body_str
-#   @parsed = JSON.parse(@data)
-#   @events = @parsed['events']
-#   erb :events
-# end
 
 $cookies = []
 $muffins = []
@@ -73,30 +62,42 @@ class Cake
 end
 
 
-cookie_one = Cookie.new("Vegeta-licious", "/images/c1.png", "The Prince of all cookies is second to none in both appearance and delciousness.", "$7.00")
+cookie_one = Cookie.new("Sugar Cookie", "/images/c1.png", "The best sugar cookies on the planet.", "$3.00")
 
-cookie_two = Cookie.new("Kakarot Sugar Crunch", "/images/c2.png", "A suitable choice, full of lots of sugar. Perfect... for a clown!", "$2.00")
+cookie_two = Cookie.new("Chocolate Frosted", "/images/c2.png", "Chocolate dream full of lots of sugar.", "$3.00")
 
-cookie_three = Cookie.new("Gohan-ana Cookie", "/images/c3.png", "This banana flavored cookie had more potential than any of the others... until it got burnt.", "$3.00")
+cookie_three = Cookie.new("Chocolate Chip", "/images/c3.png", "If you love chocolate chip... yum,yum, yum!.", "$2.00")
 
-cookie_four = Cookie.new("Super Gotenks S'more", "/images/c4.png", "A cookie that would be a lot better off working on itself instead of stupid gimmicks.", "$6.00")
+cookie_four = Cookie.new("Double Chocolate Chip Oreo", "/images/c4.png", "A cookie that looks like Oreo, or maybe DOUBlE oreo.", "$3.00")
 
-cookie_five = Cookie.new("Beerus Pudding Chip Cookie", "/images/c5.png", "Eating these will split your world in half", "$5.00")
+cookie_five = Cookie.new("Strawberry Chunks", "/images/c5.png", "If you love these cookies, then you will love my cookies", "$5.00")
 
-cookie_six = Cookie.new("Buu Buu Brownie", "/images/c6.png", "Try this oversized cookie once, and it will blow your world apart.", "$9.00")
+cookie_six = Cookie.new("Choco Choc", "/images/c6.png", "Yum, Yum, Yum You need to get cookies with nuts!.", "$3.00")
 
-muffin_one = Muffin.new("Ma-Junior Muffin", "/images/m1.png", "The Namek has always been a skilled tactician, and knows when to get out of my way.", "$3.00")
+muffin_one = Muffin.new("Choc chip Muffin", "/images/m1.png", "The Muffin man is here , and knows what you wANT1.", "$5.00")
 
-muffin_two = Muffin.new("Majin Bubble Muffin", "/images/m2.png", "We all make mistakes, some involve more wholesale slaughter than others. This muffin is free for victims of the 25th Techiaki Budokai 'disaster'.", "$3.00 | Free ")
+muffin_two = Muffin.new("Strawberry Muffin", "/images/m2.png", "Strawberry on top is the best, yum, yum, yum. This muffin is free of preservatives.", "$6.00")
 
-muffin_three = Muffin.new("Jaco", "/images/m3.png", "Where's Jaco? I killed him, and made him into muffins. WHAT?", "$4.00")
+muffin_three = Muffin.new("Choco LOoco", "/images/m4.png", "I live for muffins, do you? I know you do!", "$3.00")
 
-muffin_four = Muffin.new("Princess Bulma Spectacular", "/images/m4.png", "This is for my Bulma. You can't have one!", "N/A")
+muffin_four = Muffin.new("MUFF Muffin", "/images/m5.png", "You can't have this one or maybe!", "3.00")
 
-cake_one = Cake.new("Chocolate Gun!", "/images/ca1.png", "The wonderful flavor of this cake is world shattering.", "$17.00")
+cake_one = Cake.new("Vanilla!", "/images/ck1.png", "The wonderful flavor of this cake is the bomb.", "$47.00")
 
-cake_two = Cake.new("Vanilla Bang Cake!", "/images/ca2.png", "You're going to have to finish what you started with this cake. Best enjoyed in hell!", "$19.00")
+cake_two = Cake.new("Vanilla Chocolate Cake!", "/images/ck2.png", "I love this cake and you should too. Best enjoyed in alone!", "$49.00")
 
-cake_three = Cake.new("Final Ganache Cake!", "/images/ca3.png", "This cake takes extra long to prepare, but once you have it, it will be the last cake you ever taste!", "$23.00")
+cake_three = Cake.new("Chocolate Cake!", "/images/ck3.png", "This cake is our flagship cake, it will be the last cake you ever taste!", "$63.00")
 
-cake_four = Cake.new("Shine Shine Cake Cake", "/images/ca4.png", "Try to keep up with the flavor of this one!", "$15.00")
+cake_four = Cake.new("Strawberry Cake Cake", "/images/ck4.png", "This taste like Strawberry Shortcake!", "$65.00")
+
+cake_one = Cake.new("Chocolate Gun!", "/images/ck5.png", "I love chocolate monkeys, do you?.", "$37.00")
+
+cake_two = Cake.new("Strawberry Frosted", "/images/cc1.png", "This is quite disgusting! BUT WE SELL IT ANYWAY!", "$2.00")
+
+cake_three = Cake.new("Party Sprinkles!", "/images/cc2.png", "This cupcake takes extra long to prepare, but its worth the wait!", "$3.00")
+
+cake_four = Cake.new("Cherry on Top", "/images/cc4.png", "Let us pop your cherry!", "$5.00")
+
+cake_four = Cake.new("Shine Shine Cup  Cake", "/images/cc5.png", "Try this one, yu will like it!", "$1.00")
+
+
